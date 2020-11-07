38cb59725ae1f23f1bf5a16deb8448e7dbde822c
ramadika <rzdka18@gmail.com>
2020-11-07 16:11:33 +0700

    Poke-app

--- src/components/BaseLayout/index.js
+++ src/components/BaseLayout/index.js
@@ -1,16 +1,18 @@
 // Dependencies
-import React from 'react'
+import React, { Component } from 'react'
 // Internals
 import NavBar from 'components/BaseLayout/NavBar'
 import Footer from 'components/BaseLayout/Footer'
 
-export default function index() {
-    return (
-        <div>
-            <NavBar />
-            {this.props.children} 
-            <Footer />
-        </div>
-    )
+export default class index extends Component {
+    
+    render() {
+        return (
+            <div>
+                <NavBar />
+                {this.props.children} 
+                <Footer />
+            </div>
+        )
+    }
 }
-
